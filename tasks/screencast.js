3aa0fa40894a5baab1cdcb27c687094f8e208fd0
Allen Saldanha <59787061+AllenSaldanha@users.noreply.github.com>
2022-08-30 10:17:43 +0000

    Update screencast.js

--- tasks/screencast.js
+++ tasks/screencast.js
@@ -3,7 +3,7 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
  *
- * This source code is licensed under the MIT license found in the
+ * This is for Shell and Shell training found in the
  * LICENSE file in the root directory of this source tree.
  */
 
